5437a9b70c0a971f2a17512d6874e1d5834c9c71
John Charman <john.charman@imhotek.net>
2025-09-24 11:04:30 +0100

    Allow a "default" array in membrane config
    This takes priority over any "default<suffix>" options
    
    Signed-off-by: John Charman <john.charman@imhotek.net>

--- src/OperationManager.php
+++ src/OperationManager.php
@@ -10,12 +10,29 @@ final class OperationManager
     private array $operationMap;
     private ?string $defaultHandler;
     private ?string $defaultDto;
+    private bool $defaultUseFlattener;
+    /* @var class-string */
+    private ?string $defaultFlattener;
 
+    /**
+     * @param array<string, mixed> $config
+     */
     public function __construct(array $config)
     {
-        $this->operationMap = $config['operationMap'] ?? [];
-        $this->defaultHandler = $config['defaultHandler'] ?? null;
-        $this->defaultDto = $config['defaultDto'] ?? null;
+        $this->operationMap = $config['operationMap']
+            ?? [];
+        $this->defaultHandler = $config['default']['handler']
+            ?? $config['defaultHandler']
+            ?? null;
+        $this->defaultDto = $config['default']['dto']
+            ?? $config['defaultDto']
+            ?? null;
+        $this->defaultUseFlattener = $config['default']['useFlattener']
+            ?? $config['defaultUseFlattener']
+            ?? true;
+        $this->defaultFlattener = $config['default']['flattener']
+            ?? $config['defaultFlattener']
+            ?? null;
     }
 
     public function has(string $id): bool
@@ -25,25 +42,28 @@ final class OperationManager
 
     public function dto(string $id): string
     {
-        return $this->operationMap[$id]['dto'] ?? 
-            $this->defaultDto ??
-            throw new \Exception('No DTO defined for operation ' . $id);
+        return $this->operationMap[$id]['dto']
+            ?? $this->defaultDto
+            ?? throw new \Exception('No DTO defined for operation ' . $id);
     }
 
     public function usesFlattener(string $id): bool
     {
-        return $this->operationMap[$id]['useFlattener'] ?? true;
+        return $this->operationMap[$id]['useFlattener']
+            ?? $this->defaultUseFlattener;
     }
 
     public function flattener(string $id): string
     {
-        return $this->operationMap[$id]['flattener'] ?? Flattener::class;
+        return $this->operationMap[$id]['flattener']
+            ?? Flattener::class;
     }
 
-    public function handler(string $id)
+    /* @return class-string */
+    public function handler(string $id): string
     {
-        return $this->operationMap[$id]['handler'] ??
-            $this->defaultHandler ??
-            throw new \Exception('No handler defined for operation ' . $id);
+        return $this->operationMap[$id]['handler']
+            ?? $this->defaultHandler
+            ?? throw new \Exception('No handler defined for operation ' . $id);
     }
 }
